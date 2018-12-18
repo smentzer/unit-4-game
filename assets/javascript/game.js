bbf4830e3fcfd19374460a93fb6fc8d494a2cb35
Cayde6 <stephaniementzer@gmail.com>
2018-12-18 17:37:53 -0500

    random number created and grabbed for images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,23 +7,35 @@ var counter = 0;
 var wins = 0;
 var losses = 0;
 
-//loop to create gems for every numberOption.
-//   for (var i = 0; i < numberOptions.length; i++) {
-//     var imageCrystal = $("<img>");
-//     gemImage.addClass("gems");
-//     gemImage.attr("src", "../images/pearl.png");
-//     gemImage.attr("data-gemvalue", numberOptions[i]);
-//     $("#gems").append(gemImage);
-//   };
 
-var targetNumber = Math.floor(Math.random() * 50) + 1;
+var randomNumber = Math.floor(Math.random() * 20) + 1;
+$("#one").attr("data-gemValue", randomNumber);
+
+randomNumber = Math.floor(Math.random() * 20) + 1;
+$("#two").attr("data-gemValue", randomNumber);
+
+randomNumber = Math.floor(Math.random() * 20) + 1;
+$("#three").attr("data-gemValue", randomNumber);
+
+randomNumber = Math.floor(Math.random() * 20) + 1;
+$("#four").attr("data-gemValue", randomNumber);
+
+
+
+
+var targetNumber = Math.floor(Math.random() * 51) + 50;
 $("#number-to-guess").text(targetNumber);
+
+
 //gems on click
-$(".gems").on("click", function () {
+$(".gemImage").on("click", function () {
 
-    counter += 10;
+    counter += parseInt($(this).attr("data-gemValue"));
+    console.log(counter)
+    console.log(targetNumber)
 
-    // alert("Total Gems: " + counter); <-- i dont ndeed this anymore.
+
+    // alert("Total Gems: " + counter); <-- i dont need this any more
 
     //logic
     if (counter === targetNumber) {
@@ -31,8 +43,24 @@ $(".gems").on("click", function () {
         // If the numbers match we'll celebrate the user's win.
         wins++;
         alert("You win!");
-        // var targetNumber = Math.floor(Math.random() * 50) + 1;
-        // $("#number-to-guess").text(targetNumber);
+        targetNumber = Math.floor(Math.random() * 51) + 50;
+        $("#number-to-guess").text(targetNumber);
+        //reset
+        counter = 0;
+        var randomNumber = Math.floor(Math.random() * 20) + 1;
+       
+        $("#one").attr("data-gemValue", randomNumber);
+
+        randomNumber = Math.floor(Math.random() * 20) + 1;
+        $("#two").attr("data-gemValue", randomNumber);
+
+        randomNumber = Math.floor(Math.random() * 20) + 1;
+        $("#three").attr("data-gemValue", randomNumber);
+
+        randomNumber = Math.floor(Math.random() * 20) + 1;
+        $("#four").attr("data-gemValue", randomNumber);
+
+       
     }
     // Here we added an "else if" condition. If the user's counter ever exceeds the targetNumber...
     else if (counter >= targetNumber) {
@@ -40,22 +68,27 @@ $(".gems").on("click", function () {
         // Then they are alerted with a loss.
         losses++;
         alert("You lose!!");
-        // var targetNumber = Math.floor(Math.random() * 50) + 1;
-        // $("#number-to-guess").text(targetNumber);
+        targetNumber = Math.floor(Math.random() * 51) + 50;
+        $("#number-to-guess").text(targetNumber);
+        //reset
+        counter = 0;
+        var randomNumber = Math.floor(Math.random() * 20) + 1;
+        $("#one").attr("data-gemValue", randomNumber);
+
+        randomNumber = Math.floor(Math.random() * 20) + 1;
+        $("#two").attr("data-gemValue", randomNumber);
+
+        randomNumber = Math.floor(Math.random() * 20) + 1;
+        $("#three").attr("data-gemValue", randomNumber);
+
+        randomNumber = Math.floor(Math.random() * 20) + 1;
+        $("#four").attr("data-gemValue", randomNumber);
     }
+
     //for html
     $("#wins-text").html("Total Wins: " + wins);
     $("#losses-text").html("Total Losses: " + losses);
     $("#number-to-guess").html(targetNumber);
     $("#new-score").html("Total gems: " + counter);
-
-
-
-
-
-
-    // document.getElementById("wins-text").innerHTML = "Total Wins: " + wins;
-    // document.getElementById("losses-text").innerHTML = "Total Losses: " + losses;
-    // document.getElementById("new-score").innerHTML = "
 
 });
